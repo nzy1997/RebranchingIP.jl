8e986b700b01000dfdb30b68b808cb0612882be4
Documenter.jl <documenter@juliadocs.github.io>
2025-04-28 01:48:40 +0000

    build based on c371194

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RebranchingIP","category":"page"},{"location":"#RebranchingIP","page":"Home","title":"RebranchingIP","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RebranchingIP.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RebranchingIP]","category":"page"}]
+[{"location":"#RebranchingIP","page":"Home","title":"RebranchingIP","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RebranchingIP.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
